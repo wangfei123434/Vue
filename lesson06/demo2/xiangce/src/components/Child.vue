939e9b785216e1aa4d4ca6c6aefdb2aba07ece74
wangfei123434 <807193004@qq.com>
2020-05-18 21:44:53 +0800

    添加了相册demo

---
+++ lesson06/demo2/xiangce/src/components/Child.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<h1>多彩照片墙相册</h1>
+		<ul>
+			<li v-for="(item,index) in imgall">
+				<img :src="item"/>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:"Child",
+		props:["imgall"]
+	}
+</script>
+
+<style>
+	ul{
+		margin: 30px auto;
+		width: 1200px;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	li{
+		list-style: none;
+		margin: 10px;
+	}
+</style>
--- lesson06/demo2/xiangce/src/components/HelloWorld.vue
+++ lesson06/demo2/xiangce/src/components/HelloWorld.vue
@@ -1,95 +1,29 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <child :imgall="imgsrc"></child>
   </div>
 </template>
 
 <script>
+import child from "./Child"
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      imgsrc:[
+		  require("../assets/img/1.jpg"),
+		  require("../assets/img/2.jpg"),
+		  require("../assets/img/3.jpg"),
+		  require("../assets/img/4.jpg"),
+		  require("../assets/img/5.jpg"),
+		  require("../assets/img/6.jpg"),
+		  require("../assets/img/7.png"),
+		  require("../assets/img/8.png")
+	  ]
     }
+  },
+  components:{
+  	child
   }
 }
 </script>
