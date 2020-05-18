e5f112f7f55bc931951091a7e990af692bba3db5
wangfei123434 <807193004@qq.com>
2020-05-18 18:06:10 +0800

    更新了文件

---
+++ lesson06/demo1/src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<h1>我是子组件</h1>
+		<h3>子组件接收到父组件传过来的值是：{{ inpmsg }}</h3>
+		<br />
+		<button @click="fn">点击我传递数据给父组件</button>
+	</div>
+</template>
+
+<script>
+	//导出child
+	export default {
+		name:"Child",
+		props:["inpmsg"],
+		data () {
+			return {
+				childMsg:"我是子组件的data数据"
+			}
+		},
+		methods:{
+			fn(){
+				this.$emit("change",this.childMsg)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ lesson06/demo1/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="msg" @keypress.enter="alertFn"/>
+    <h1 class="redH1"> {{ toupper }}</h1>
+    
+    <child inpmsg="我是父组件的data数据" @change="getValue"></child>
+  </div>
+</template>
+
+<script>
+//引入child:
+import child from "./Child"
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'abcd'
+    }
+  },
+  computed:{
+  	toupper(){
+  		return this.msg.toUpperCase()
+  	}
+  },
+  methods:{
+  	alertFn(){
+  		alert(this.msg)
+  	},
+  	getValue(val){
+  		console.log(val)
+  	}
+  	
+  },
+  components:{
+  	child
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.redH1{
+	color: red;
+}
+</style>
